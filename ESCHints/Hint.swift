b31366eef9a63e503a349a5227e03bab4030e7bd
Matt Ao <=>
2018-04-19 16:33:32 +0300

    hint class

---
+++ ESCHints/Hint.swift
@@ -0,0 +1,53 @@
+//
+//  Hint.swift
+//  ESCHints
+//
+//  Created by Matt Ao on 4/19/18.
+//  Copyright © 2018 Matt Ao. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class Hint: NSObject {
+    var identifier: CKRecordID?
+    var room: String?
+    var hintString: String?
+    
+    override init() {
+        if let roomString = UserDefaults.value(forKey: "room") as? String {
+            room = roomString
+        } else {
+            print("Error: No Room Set")
+        }
+    }
+    
+    init(record: CKRecord) {
+        if let room = record.value(forKey: "room") as? String {
+            self.room = room
+        }
+        if let hintString = record.value(forKey: "hintString") as? String {
+            self.hintString = hintString
+        }
+        self.identifier = record.recordID
+    }
+    
+    func record() -> CKRecord? {
+        var record: CKRecord
+        if let id = identifier {
+            record = CKRecord(recordType: HintType, recordID: id)
+        } else {
+            record = CKRecord(recordType: HintType)
+        }
+        
+        record.setValue(room, forKey:"room")
+        record.setValue(hintString, forKey: "hintString")
+        
+        return record
+    }
+    
+    static func ==(first: Hint, second: Hint) -> Bool {
+        return first.identifier == second.identifier
+    }
+    
+}
