320465404358abd3de6e3b07ca3907ae37a293ed
Matt Ao <=>
2018-04-24 08:10:16 -0400

    subscription for hints

--- ESCHints/AppDelegate.swift
+++ ESCHints/AppDelegate.swift
@@ -7,18 +7,60 @@
 //
 
 import UIKit
+import CloudKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { authorized, error in
+            if authorized {
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            }
+        })
+        
         return true
     }
 
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let notificationInfo = CKNotificationInfo()
+        notificationInfo.alertBody = "👌🏻👌🏻"
+        notificationInfo.shouldBadge = false
+        notificationInfo.soundName = "default"
+        notificationInfo.title = "A Hint Just For You"
+        if let currentRoom = UserDefaults().value(forKey: "room") as? String {
+            let predicate = NSPredicate(format: "room = %@", currentRoom)
+            let subscription = CKQuerySubscription(recordType: "Hint", predicate: predicate, options: .firesOnRecordCreation)
+            subscription.notificationInfo = notificationInfo
+            
+            let publicDB = CKContainer.init(identifier: "iCloud.esc.GameMaster").publicCloudDatabase
+            publicDB.save(subscription, completionHandler: { subscription, error in
+                if error == nil {
+                    print("success")
+                } else {
+                    print("fail")
+                }
+            })
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+        
+        if UIApplication.shared.applicationState == .active {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.reloadData()
+            }
+        }
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -33,8 +75,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
+    func reloadData() {
+        if let mainVC = self.window?.rootViewController as? ViewController {
+            mainVC.fetchAllHints()
+        }
+    }
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        reloadData()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
